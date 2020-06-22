631322fa1f3b328626374234e56cf2683facb043
jonasllaurinai@gmail.com <jonasllaurinai@gmail.com>
2020-06-22 23:08:47 +0300

    db conn

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
+# App and db inits, configs
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '91f3cd4f5c6b411e7c60794b3d9d31af'
+app.config['DATABASE_URI'] = 'sqlite:///site.db'
+DATABASE = 'site.db'
+db = SQLAlchemy(app)
+
+# Specify user table
+class User(db.Model):
+    id 
 
 
+# Website's routes
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('/home.html')
 
-
 @app.route('/about')
 def about():
     return render_template('/about.html')
 
-
 @app.route('/blog')
 def blog():
     return render_template('/blog.html')
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -28,7 +35,6 @@ def register():
         flash(f'Account created for {form.username.data}!',  'primary')
         return redirect(url_for('index'))
     return render_template('/includes/_register.html', title='Register', form=form)
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -42,4 +48,6 @@ def login():
     return render_template('/includes/_login.html', title='Login', form=form)
 
 
-app.run(debug=True)
+# Run directly
+if __name__ == '__main__':
+    app.run(debug=True)
