b36ef73f0b1751d039e1d4126868820af2f39772
jonasllaurinai@gmail.com <jonasllaurinai@gmail.com>
2020-06-25 18:31:21 +0300

    few modf

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 
 # This file contains instructions for flask app
+
+
 class Configuration(object):
     APP_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
-    SQLALCHEMY_DB_URI = 'sqliteext:///%s' % os.path.join(APP_DIR, "site.db")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % os.path.join(APP_DIR, "site.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
