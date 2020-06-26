eff009a274a010d3705ad03951282dee80a5cb25
jonasllaurinai@gmail.com <jonasllaurinai@gmail.com>
2020-06-26 13:29:18 +0300

    adding migrations to db

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import (Flask, render_template, url_for, flash, redirect, abort,
                     request, Response, session, Markup)
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Configuration
@@ -11,7 +13,11 @@ app.config.from_object(Configuration)
 
 # Init sqlalchemy for managing db
 db = SQLAlchemy(app)
+import models
 
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 # SECRET_KEY = '91f3cd4f5c6b411e7c60794b3d9d31af'
 
